b0762266229c5476f2485c35dd496e82b6909649
jpeeler <jpeeler@gmail.com>
2023-08-10 14:00:32 -0400

    add waybar-git

--- scripts/pre/overrides.sh
+++ scripts/pre/overrides.sh
@@ -1,3 +1,4 @@
 set -oue pipefail
 
 rpm-ostree override --experimental replace xorg-x11-server-Xwayland --from repo=copr:copr.fedorainfracloud.org:solopasha:hyprland
+rpm-ostree override --experimental replace waybar-git --from repo=copr:copr.fedorainfracloud.org:solopasha:hyprland --remove waybar
